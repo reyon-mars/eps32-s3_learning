06d053871733e31d4411338c40e4e7bd18a5d3f6
Reyon Rai <reyonrai50@gmail.com>
2025-07-29 14:10:47 +0545

    led_pwm_fade project added and completed.

--- button_controlled_led/main/button_controlled_led.c
+++ button_controlled_led/main/button_controlled_led.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_GPIO GPIO_NUM_10
+#define BUTTON_GPIO GPIO_NUM_9
+
+#define DEBOUNCE_DELAY 50
+
 
 void app_main(void)
 {
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
 
+    gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
+    gpio_pullup_en(BUTTON_GPIO);
+    gpio_pulldown_dis(BUTTON_GPIO); 
+
+    int led_state = 0;
+    int last_button_state = 1;
+
+    while (1) {
+        int button_state = gpio_get_level(BUTTON_GPIO);
+
+        if (last_button_state == 1 && button_state == 0) {
+            vTaskDelay(pdMS_TO_TICKS(DEBOUNCE_DELAY));
+            button_state = gpio_get_level(BUTTON_GPIO);
+
+            if (button_state == 0) {
+                led_state = !led_state;
+                gpio_set_level(LED_GPIO, led_state);
+                printf("Button pressed. LED is now %s\n", led_state ? "ON" : "OFF");
+            }
+        }
+        last_button_state = button_state;
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
 }
---
+++ led_pwm_fade/main/led_pwm_fade.c
@@ -0,0 +1,55 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+
+#define LEDC_TIMER          LEDC_TIMER_0
+#define LEDC_MODE           LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO      (2)
+#define LEDC_CHANNEL        LEDC_CHANNEL_0
+#define LEDC_DUTY_RES       LEDC_TIMER_13_BIT
+#define LED_FREQUENCY       (1000)
+
+static const char* TAG = "PWM_FADE";
+
+void app_main(void)
+{
+    ESP_LOGI( TAG, "Configuring LEDC PWM....");
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode = LEDC_MODE,
+        .timer_num = LEDC_TIMER,
+        .duty_resolution = LEDC_DUTY_RES,
+        .freq_hz = LED_FREQUENCY,
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+    ledc_timer_config( &ledc_timer );
+
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode = LEDC_MODE,
+        .channel = LEDC_CHANNEL,
+        .timer_sel = LEDC_TIMER,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = LEDC_OUTPUT_IO, 
+        .duty = 0,
+        .hpoint = 0
+    };
+    ledc_channel_config( &ledc_channel );
+
+    while( 1 ){
+        ESP_LOGI( TAG, "Fading in.... " );
+        for( int duty = 0; duty <= 8191; duty += 20 ){
+            ledc_set_duty( LEDC_MODE, LEDC_CHANNEL, duty );
+            ledc_update_duty( LEDC_MODE, LEDC_CHANNEL );
+            vTaskDelay( pdMS_TO_TICKS( 10 ) );
+        }
+
+        ESP_LOGI( TAG, "Fading out...." );
+        for( int duty = 8191; 0 <= duty; duty -= 20 ){
+            ledc_set_duty( LEDC_MODE, LEDC_CHANNEL, duty );
+            ledc_update_duty( LEDC_MODE, LEDC_CHANNEL );
+            vTaskDelay( pdMS_TO_TICKS( 10 ));
+        }
+    }
+}
