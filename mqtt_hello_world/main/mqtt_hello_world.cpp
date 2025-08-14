2dc1cd87d01754740110096ec3279f401030fc1b
Reyon Rai <reyonrai50@gmail.com>
2025-08-14 17:15:27 +0545

    mqtt hello_world project started

---
+++ mqtt_hello_world/main/mqtt_hello_world.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <cstring>
+#include <string_view>
+
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "mqtt_client.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+
+static constexpr const char* TAG = "MQTT_HELLO_WORLD";
+
+
+
+void app_main(void)
+{
+
+}
